12257965e1ebed198fc6bd52abfcffece2f24b47
carranza96 <manuelcarranza96@gmail.com>
2023-03-01 16:31:43 +0100

    CBGSDataset added in confs

--- scripts/nuscenes_train.sh
+++ scripts/nuscenes_train.sh
@@ -1,14 +1,19 @@
 MODEL=centerpoint
-RESULTS_DIR=results/nuscenes/${MODEL}/nd_pillar
+RESULTS_DIR=results/nuscenes/${MODEL}02/40epochs_cbgs/4xb4/nd_pillar
 CONFIG_FILE=configs/centerpoint_temporal/centerpoint_02pillar_nd_second_secfpn_4x8_cyclic_20e_nus.py
 
-./tools/dist_train.sh ${CONFIG_FILE} 2 --work-dir=${RESULTS_DIR}
-
+./tools/dist_train.sh ${CONFIG_FILE} 4 --work-dir=${RESULTS_DIR}
 
 
 MODEL=centerpoint
-RESULTS_DIR=results/nuscenes/${MODEL}/nd_pillar_convlstm
-CONFIG_FILE=configs/centerpoint_temporal/centerpoint_02pillar_nd_temporal_second_secfpn_4x8_cyclic_20e_nus.py
+RESULTS_DIR=results/nuscenes/${MODEL}02/40epochs_cbgs/4xb4/nd_pillar_transf
+CONFIG_FILE=configs/centerpoint_temporal/centerpoint_02pillar_nd_transformer_second_secfpn_4x8_cyclic_20e_nus.py
 
-./tools/dist_train.sh ${CONFIG_FILE} 2 --work-dir=${RESULTS_DIR}
+./tools/dist_train.sh ${CONFIG_FILE} 4 --work-dir=${RESULTS_DIR}
 
+
+MODEL=centerpoint
+RESULTS_DIR=results/nuscenes/${MODEL}02/40epochs_cbgs/4xb4/nd_pillar_convlstm
+CONFIG_FILE=configs/centerpoint_temporal/centerpoint_02pillar_nd_convlstm_second_secfpn_4x8_cyclic_20e_nus.py
+
+./tools/dist_train.sh ${CONFIG_FILE} 4 --work-dir=${RESULTS_DIR}
